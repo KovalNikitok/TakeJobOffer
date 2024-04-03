d00e4ccb5369f9012c9d4086657400604271e543
Никита Коваль <nikitakovalmen2@gmail.com>
2024-04-04 02:03:46 +0600

    Added Profession-Skill many-to-many-table, domain model, repo, service and controller with some usable methods

---
+++ TakeJobOffer.DAL/Repositories/ProfessionsSkillsRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TakeJobOffer.DAL.Entities;
+using TakeJobOffer.Domain.Models;
+
+namespace TakeJobOffer.DAL.Repositories
+{
+    public class ProfessionsSkillsRepository
+    {
+        private readonly TakeJobOfferDbContext _dbContext;
+        public ProfessionsSkillsRepository(TakeJobOfferDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<ProfessionSkill?>?> GetSkillsByProfessionId(Guid professionId)
+        {
+            var professionSkillsEntities = await _dbContext.Professions
+                .AsNoTracking()
+                .Where(p => p.Id == professionId)
+                .Select(p => p.ProfessionSkills)
+                .SingleOrDefaultAsync();
+
+            var professionSkills = professionSkillsEntities?
+                .Select(ps =>
+                {
+                    var professionSkillEntity = ProfessionSkill.CreateProfessionSkill(
+                        professionId,
+                        ps.SkillForeignKey,
+                        ps.SkillMentionCount);
+
+                    if (professionSkillEntity.IsSuccess)
+                        return professionSkillEntity.Value;
+                    return null;
+                })
+                .ToList();
+
+            return professionSkills;
+        }
+
+        public async Task<Guid> CreateSkillForProfessionById(ProfessionSkill professionSkill)
+        {
+            var professionEntity = await _dbContext.Professions
+                .AsNoTracking()
+                .Where(p => p.Id == professionSkill.ProfessionId)
+                .SingleAsync();
+
+            var professionSkillEntity = new ProfessionSkillEntity
+            {
+                ProfessionForeignKey = professionSkill.ProfessionId,
+                SkillForeignKey = professionSkill.SkillId,
+                SkillMentionCount = professionSkill.SkillMentionCount
+            };
+
+            professionEntity.ProfessionSkills.Add(professionSkillEntity);
+            await _dbContext.SaveChangesAsync();
+
+            return professionEntity.Id;
+        }
+
+
+    }
+}
---
+++ TakeJobOffer.Domain/Models/ProfessionSkills.cs
@@ -0,0 +1,34 @@
+﻿using FluentResults;
+
+namespace TakeJobOffer.Domain.Models
+{
+    public class ProfessionSkills
+    {
+        public const int MIN_PROFESION_SKILL_MENTION = 0;
+        private ProfessionSkills(Guid professionId, Guid skillId, int skillMentionCount)
+        {
+            ProfessionId = professionId;
+            SkillId = skillId;
+            SkillMentionCount = skillMentionCount;
+        }
+
+        public Result<ProfessionSkills> CreateProfessionSkills(Guid professionId, Guid skillId, int skillMentionCount)
+        {
+            var result = new Result<ProfessionSkills>();
+
+            if (professionId == Guid.Empty || skillId == Guid.Empty)
+                result.WithError($"{nameof(ProfessionSkills)} need to have not empty profession Id and skill Id");
+            if (SkillMentionCount < MIN_PROFESION_SKILL_MENTION)
+                result.WithError($"SkillMentionCount can not be less then {MIN_PROFESION_SKILL_MENTION}");
+
+            if (result.IsSuccess)
+                result.WithValue(new ProfessionSkills(professionId, skillId, skillMentionCount));
+
+            return result;
+        }
+
+        public Guid ProfessionId {  get; }
+        public Guid SkillId { get; }
+        public int SkillMentionCount { get; } = MIN_PROFESION_SKILL_MENTION;
+    }
+}
