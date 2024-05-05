a407ea86f6bdffe99cf978e0d0d24e782519b6b8
Никита Коваль <nikitakovalmen2@gmail.com>
2024-05-06 00:24:47 +0600

    Returned interface to git repository

---
+++ TakeJobOffer.Domain/Abstractions/IProfessionsSkillsRepository.cs
@@ -0,0 +1,12 @@
+﻿using TakeJobOffer.Domain.Models;
+
+namespace TakeJobOffer.Domain.Abstractions
+{
+    public interface IProfessionsSkillsRepository
+    {
+        Task<Guid?> CreateProfessionSkillById(ProfessionSkill professionSkill);
+        Task<Guid> DeleteProfessionSkillById(Guid professionId, Guid skillId);
+        Task<List<ProfessionSkill?>?> GetProfessionSkillsById(Guid professionId);
+        Task<Guid?> UpdateSkillMentionById(Guid professionId, Guid skillId, int skillMentionCount);
+    }
+}
